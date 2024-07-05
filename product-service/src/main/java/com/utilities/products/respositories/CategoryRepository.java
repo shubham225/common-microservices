d883be29535550921138e6ea772ebefc57c7552f
Shubham Shinde <shubhamshinde225@gmail.com>
2024-07-05 23:17:26 +0530

    Category Service updated

--- product-service/src/main/java/com/utilities/products/respositories/CategoryRepository.java
+++ product-service/src/main/java/com/utilities/products/respositories/CategoryRepository.java
@@ -4,8 +4,13 @@ import com.utilities.products.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    List<Category> findAll();
+    Optional<Category> findById(UUID id);
 }
--- product-service/src/main/java/com/utilities/products/services/CategoryServiceImpl.java
+++ product-service/src/main/java/com/utilities/products/services/CategoryServiceImpl.java
@@ -2,36 +2,76 @@ package com.utilities.products.services;
 
 import com.utilities.products.dtos.CategoryRequestDto;
 import com.utilities.products.models.Category;
+import com.utilities.products.respositories.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
     @Override
     public List<Category> getAllCategories() {
-        return null;
+        return categoryRepository.findAll();
     }
 
     @Override
     public Category createNewCategory(CategoryRequestDto requestDto) {
-        return null;
+        Category category = new Category();
+
+        category.setName(requestDto.getName());
+        category.setDescription(requestDto.getDescription());
+
+        category = categoryRepository.save(category);
+
+        return category;
     }
 
     @Override
     public Category getCategoryById(UUID id) {
-        return null;
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+
+        if(categoryOptional.isEmpty())
+            throw new RuntimeException("Category not Found");
+
+        return categoryOptional.get();
     }
 
     @Override
     public Category updateCategoryById(UUID id, CategoryRequestDto requestDto) {
-        return null;
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+
+        if(categoryOptional.isEmpty())
+            throw new RuntimeException("Category not Found");
+
+        Category category = categoryOptional.get();
+
+        category.setDescription(requestDto.getDescription());
+
+        category = categoryRepository.save(category);
+
+        return category;
     }
 
     @Override
     public Category deleteCategoryById(UUID id) {
-        return null;
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+
+        if(categoryOptional.isEmpty())
+            throw new RuntimeException("Category not Found");
+
+        Category category = categoryOptional.get();
+
+        categoryRepository.delete(category);
+
+        return category;
     }
 
 }
