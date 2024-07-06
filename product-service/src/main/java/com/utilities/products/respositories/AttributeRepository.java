a8a78778b1599a7b4322aca35917df12fd8acf73
Shubham Shinde <shubhamshinde225@gmail.com>
2024-07-06 23:14:23 +0530

    Attribute Service updated

--- product-service/src/main/java/com/utilities/products/respositories/AttributeRepository.java
+++ product-service/src/main/java/com/utilities/products/respositories/AttributeRepository.java
@@ -4,8 +4,12 @@ import com.utilities.products.models.Attribute;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AttributeRepository extends JpaRepository<Attribute, UUID> {
+    List<Attribute> findAll();
+    Optional<Attribute> findById(UUID id);
 }
--- product-service/src/main/java/com/utilities/products/services/AttributeServiceImpl.java
+++ product-service/src/main/java/com/utilities/products/services/AttributeServiceImpl.java
@@ -1,38 +1,76 @@
 package com.utilities.products.services;
 
 import com.utilities.products.dtos.AttributeRequestDto;
+import com.utilities.products.enums.AttributeType;
 import com.utilities.products.models.Attribute;
 import com.utilities.products.models.AttributeTerm;
+import com.utilities.products.respositories.AttributeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class AttributeServiceImpl implements AttributeService{
+
+    private final AttributeRepository attributeRepository;
+
+    public AttributeServiceImpl(AttributeRepository attributeRepository) {
+        this.attributeRepository = attributeRepository;
+    }
+
     @Override
     public List<Attribute> getAllAttributes() {
-        return null;
+        return attributeRepository.findAll();
     }
 
     @Override
     public Attribute createNewAttribute(AttributeRequestDto requestDto) {
-        return null;
+        Attribute attribute = new Attribute();
+        attribute.setName(requestDto.getName());
+        attribute.setType(AttributeType.SELECT);
+
+        attribute = attributeRepository.save(attribute);
+
+        return attribute;
     }
 
     @Override
     public Attribute getAttributeById(UUID id) {
-        return null;
+        Optional<Attribute> attributeOptional = attributeRepository.findById(id);
+
+        if(attributeOptional.isEmpty())
+            throw new RuntimeException("Attribute not found");
+
+        return attributeOptional.get();
     }
 
     @Override
     public Attribute updateAttributeById(UUID id, AttributeRequestDto requestDto) {
-        return null;
+        Optional<Attribute> attributeOptional = attributeRepository.findById(id);
+
+        if(attributeOptional.isEmpty())
+            throw new RuntimeException("Attribute not found");
+
+        Attribute attribute = attributeOptional.get();
+        attribute.setName(requestDto.getName());
+        attribute = attributeRepository.save(attribute);
+
+        return attribute;
     }
 
     @Override
     public Attribute deleteAttributeById(UUID id) {
-        return null;
+        Optional<Attribute> attributeOptional = attributeRepository.findById(id);
+
+        if(attributeOptional.isEmpty())
+            throw new RuntimeException("Attribute not found");
+
+        Attribute attribute = attributeOptional.get();
+        attributeRepository.delete(attribute);
+
+        return attribute;
     }
 
     @Override
