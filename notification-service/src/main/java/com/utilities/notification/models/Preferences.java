dcf7ddba69ac0fc6305dbe9ae868703b70d03d6c
Shubham Shinde <shubhamshinde225@gmail.com>
2024-07-18 21:43:06 +0530

    UserPreferencesDto added

--- notification-service/src/main/java/com/utilities/notification/models/Preferences.java
+++ notification-service/src/main/java/com/utilities/notification/models/Preferences.java
@@ -4,9 +4,14 @@ import com.utilities.notification.enums.NotificationType;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Setter
 @Getter
 public class Preferences {
-    private NotificationType notificationType;
-    private EnabledChannels enabledChannels;
+    private UUID                userId;
+    private NotificationType    notificationType;
+    private boolean             email;
+    private boolean             sms;
+    private boolean             inApp;
 }
